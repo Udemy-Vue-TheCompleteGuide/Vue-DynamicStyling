53423a289fca6c296575559cc263f1f53f78cdcc
Marlon Fernandez <marlonfsolis@outlook.com>
2021-01-05 23:37:39 -0500

    Adding style dynamically.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Styling></Styling>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Styling from "@/components/Styling";
 
 export default {
   name: 'App',
   components: {
+    Styling,
+
+    // eslint-disable-next-line vue/no-unused-components
     HelloWorld
   }
 }
@@ -24,5 +27,51 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #4fc08d;
+  color: white;
+  text-align: center;
+}
+
+#styling {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+}
+
+.demo {
+  width: calc(100% - 32px);
+  height: 100px;
+  margin: 16px;
+  border: 2px dashed #ccc;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.active {
+  border-color: red;
+  background-color: salmon;
 }
 </style>
---
+++ src/components/Styling.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <header>
+      <h1>Vue Dynamic Styling</h1>
+    </header>
+    <section id="styling">
+      <div class="demo"
+           :style="{borderColor: box1 ? 'red' : '#ccc'}"
+           id="1"
+           @click="selectBox(1)"
+      >Inline styling</div>
+      <div class="demo"
+           :class="{demo:true, active: box2}"
+           id="2"
+           @click="selectBox(2)"
+      >Adding classes dynamically</div>
+      <div class="demo"
+           :class="box3Classes"
+           id="3"
+           @click="selectBox(3)"
+      > Classes & Computed Properties</div>
+      <div :class="['demo', {active: box4}]"
+           id="4"
+           @click="selectBox(4)"
+      >Adding classes dynamically with Array Syntax</div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Styling",
+  data() {
+    return {
+      box1: false,
+      box2: false,
+      box3: false,
+      box4: false,
+    }
+  },
+  computed: {
+    box3Classes() {
+      // Here we can have a more complex logic and have the html cleaner.
+      return {active: this.box3}
+    }
+  },
+  methods: {
+    selectBox(boxId) {
+      switch (boxId) {
+        case 1:
+          this.box1 = !this.box1;
+          break;
+        case 2:
+          this.box2 = !this.box2;
+          break
+        case 3:
+          this.box3 = !this.box3;
+          break;
+        case 4:
+          this.box4 = !this.box4;
+          break;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
